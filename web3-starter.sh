3af03c309f78aa90f6cedaffcdcc3e3fd4d175c5
Greg <gskril@gmail.com>
2023-02-06 00:58:41 +0200

    Add web3-starter template generator

---
+++ web3-starter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Generate web3-starter
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon ⚒️
+# @raycast.argument1 { "type": "text", "placeholder": "Repo name" }
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description Generate new repo from web3-starter template
+# @raycast.author Greg Skriloff
+# @raycast.authorURL https://gregskril.com
+
+echo "Creating repo from web3-starter template..."
+cd /Users/Greg/Desktop/crypto/ || exit
+gh repo create "$1" --template gskril/web3-starter --private
+sleep 3
+echo "https://github.com/gskril/$1" | pbcopy
+mkdir /Users/Greg/Desktop/crypto/"$1"
+cd /Users/Greg/Desktop/crypto/"$1" || exit
+gh repo clone gskril/"$1"
+cd "$1" || exit
+echo "Launching VS Code..."
+code .
+yarn install
