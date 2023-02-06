122c271087eab99c608e73a1c6d9c9f98ba4b387
Greg <gskril@gmail.com>
2023-02-06 13:17:43 +0200

    Simplify sharecaster command

--- sharecaster.js
+++ sharecaster.js
@@ -14,28 +14,23 @@
 // @raycast.author Greg Skriloff
 // @raycast.authorURL https://gregskril.com
 
+import axios from 'axios'
 import clipboard from 'clipboardy'
 
 const deeplink = process.argv.slice(2)[0]
 
-const { data } = await fetch('https://sharecaster.xyz/api/share', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    sharecast: deeplink,
-  }),
-})
-  .then((res) => {
-    if (!res.ok) throw new Error('Error creating sharecaster URL')
-    return res.json()
-  })
-  .catch(() => {
-    throw new Error('Error creating sharecaster URL')
-  })
+try {
+  const { data: sharecaster } = await axios.post(
+    'https://sharecaster.xyz/api/share',
+    {
+      sharecast: deeplink,
+    }
+  )
 
-const sharecaster = `https://sharecaster.xyz/${data}`
-clipboard.writeSync(sharecaster)
+  const url = `https://sharecaster.xyz/${sharecaster.data}`
+  clipboard.writeSync(url)
 
-console.log('Copied Sharecaster URL to clipboard')
+  console.log('Copied Sharecaster URL to clipboard')
+} catch {
+  console.log('Error creating Sharecaster URL')
+}
