0f5c54bd0de586474d579e7961fefb59d60e9887
Greg <gskril@gmail.com>
2023-03-15 00:00:24 -0400

    Add typescript template generator

---
+++ typescript-template.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Generate Typescript project
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⚒️
+# @raycast.argument1 { "type": "text", "placeholder": "Folder name" }
+# @raycast.packageName Developer utils
+
+# Documentation:
+# @raycast.author Greg Skriloff
+# @raycast.authorURL https://gregskril.com
+
+cp -r /Users/Greg/Documents/typescript-template /Users/Greg/Desktop
+mv /Users/Greg/Desktop/typescript-template /Users/Greg/Desktop/"$1"
+cd /Users/Greg/Desktop/"$1" || exit
+echo "Launching VS Code..."
+code .
